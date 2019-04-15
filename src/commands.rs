83e055939c59b4d97d3d68266a33a931f472e03d
Tearzz <benjamin.stigsen@gmail.com>
2019-04-15 21:09:53 +0200

    Added API commands

--- src/commands.rs
+++ src/commands.rs
@@ -1,5 +1,6 @@
 use std::io::prelude::*;
 use std::fs::File;
+extern crate reqwest;
 
 // Handle Commands
 pub fn handle_command(user: &str, msg: &str) -> String {
@@ -14,6 +15,9 @@ pub fn handle_command(user: &str, msg: &str) -> String {
 
 		"ping" 				=> "Pong!".to_string(),
 		"hug" 				=> format!("/me hugs {}!", user),
+
+		// API
+		"uptime"			=> uptime(),
 
 		// DEFAULT
 		_ 					=> format!("{} is not a recognized command!", data[0])
@@ -39,6 +43,14 @@ fn response_from_file() -> String {
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Unable to read the file");
 
-    //crate::send_msg(&contents.replace("\n", " ").replace("\r", ""))
     contents.replace("\n", " ").replace("\r", "")
+}
+
+// API \\
+fn uptime() -> String {
+	let url = format!("https://decapi.me/twitch/uptime/{}", *crate::CHAN);
+
+ 	let response = reqwest::get(&url).expect("Error").text().unwrap();
+
+    response
 }
